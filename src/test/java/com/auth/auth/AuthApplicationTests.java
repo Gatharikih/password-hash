633c2ef71590cab6dcaf2c6838b56912a72e2ee0
Gatharikih <sgatharikih@gmail.com>
2023-01-24 14:11:19 +0300

    update contextLoads test

--- src/test/java/com/auth/auth/AuthApplicationTests.java
+++ src/test/java/com/auth/auth/AuthApplicationTests.java
@@ -1,13 +1,20 @@
 package com.auth.auth;
 
+import com.auth.auth.controllers.MainController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AuthApplicationTests {
+    @Autowired
+    private MainController controller;
 
     @Test
     void contextLoads() {
+        assertThat(controller).isNotNull();
     }
 
 }
